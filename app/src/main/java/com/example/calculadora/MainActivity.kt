c698b5a3066136c5574bb389e64acb013df1cf51
samuel54Solano <127533662+samuel54solano@users.noreply.github.com>
2025-01-21 16:49:10 -0500

    Initial commit

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.calculadora
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Switch
 import com.example.calculadora.databinding.ActivityMainBinding
+import java.util.stream.Stream
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    var num1: Any = 0
+    var num2: Any = 0
+    var textPanttalla = "0"
+    var operacion = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -13,32 +22,58 @@ class MainActivity : AppCompatActivity() {
         pintar()
     }
 
-    fun pintar() {
+    private fun pintar() {
         binding.apply {
-
-
-            btn1.setOnClickListener { txtPantalla.text="1" }
-//            btn2.setOnClickListener { quitarCero; numPresionado(num = "2") }
-//            btn3.setOnClickListener { quitarCero; numPresionado(num = "3") }
-//            btn4.setOnClickListener { quitarCero; numPresionado(num = "4") }
-//            btn5.setOnClickListener { quitarCero; numPresionado(num = "5") }
-//            btn6.setOnClickListener { quitarCero; numPresionado(num = "6") }
-//            btn7.setOnClickListener { quitarCero; numPresionado(num = "7") }
-//            btn8.setOnClickListener { quitarCero; numPresionado(num = "8") }
-//            btn9.setOnClickListener { quitarCero; numPresionado(num = "9") }
-//            btn0.setOnClickListener { quitarCero; numPresionado(num = "0") }
-//            btnSuma.setOnClickListener { quitarCero; oprcnPresionada(Suma) }
-//            btnResta.setOnClickListener { quitarCero; oprcnPresionada(Resta) }
-//            btnDivison.setOnClickListener { quitarCero; oprcnPresionada(Division) }
-//            btnMultiplicacion.setOnClickListener { quitarCero; oprcnPresionada(Multiplicacion) }
+            btn1.setOnClickListener { quitarCero(); agregarNumero(num = "1") }
+            btn2.setOnClickListener { quitarCero(); agregarNumero(num = "2") }
+            btn3.setOnClickListener { quitarCero(); agregarNumero(num = "3") }
+            btn4.setOnClickListener { quitarCero(); agregarNumero(num = "4") }
+            btn5.setOnClickListener { quitarCero(); agregarNumero(num = "5") }
+            btn6.setOnClickListener { quitarCero(); agregarNumero(num = "6") }
+            btn7.setOnClickListener { quitarCero(); agregarNumero(num = "7") }
+            btn8.setOnClickListener { quitarCero(); agregarNumero(num = "8") }
+            btn9.setOnClickListener { quitarCero(); agregarNumero(num = "9") }
+            btn0.setOnClickListener { agregarNumero(num = "0") }
+            btnClear.setOnClickListener { clear() }
+//            btnSuma.setOnClickListener { oprcnPresionada(opecion = "+") }
+//            btnResta.setOnClickListener { oprcnPresionada(opecion = "-") }
+//            btnDivison.setOnClickListener { oprcnPresionada(opecion = "/") }
+//            btnMultiplicacion.setOnClickListener { oprcnPresionada(opecion = "x") }
 
         }
     }
 
 
+    private fun agregarNumero(num: String) {
 
+        if (textPanttalla == "0" && num == "0") {
+            return
+        }
+        if (textPanttalla == "0" && num != "0") {
+            textPanttalla = num
+        } else {
+            textPanttalla += num
+        }
+        binding.txtPantalla.text = textPanttalla
 
+    }
 
+    private fun clear() {
+        num1 = 0
+        num2 = 0
+        textPanttalla = ""
+        binding.txtPantalla.text = "0"
+        operacion = ""
+        binding.txtHistorial.text = ""
+    }
+
+    private fun quitarCero() {
+        binding.apply {
+            if (txtPantalla.text == "0") {
+                txtPantalla.text = "0"
+            }
+        }
+    }
 
 
 }
