c82238b0ed435644b580990bf98e9d3440d5972e
SamuelSolanows <Samuelsolanoama@gmail.com>
2025-01-21 17:13:55 -0500

    logica de calculo

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -10,9 +10,9 @@ import java.util.stream.Stream
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-    var num1: Any = 0
-    var num2: Any = 0
-    var textPanttalla = "0"
+    var num1: Any = 0.0
+    var num2: Any = 0.0
+    var textPanttalla = ""
     var operacion = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,11 +35,31 @@ class MainActivity : AppCompatActivity() {
             btn9.setOnClickListener { quitarCero(); agregarNumero(num = "9") }
             btn0.setOnClickListener { agregarNumero(num = "0") }
             btnClear.setOnClickListener { clear() }
-//            btnSuma.setOnClickListener { oprcnPresionada(opecion = "+") }
+///            btnSuma.setOnClickListener {}
 //            btnResta.setOnClickListener { oprcnPresionada(opecion = "-") }
 //            btnDivison.setOnClickListener { oprcnPresionada(opecion = "/") }
 //            btnMultiplicacion.setOnClickListener { oprcnPresionada(opecion = "x") }
+//            btnResultado.setOnClickListener {  calcular(
+//                num1.toString().toDouble(), num2.toString().toDouble(), operacion
+//            ) }
 
+        }
+    }
+
+    private fun calcular(num1: Double, num2: Double, operador: String): Double {
+        return when (operador) {
+            "+" ->  num1 + num2
+            "-" -> num1 - num2
+            "X" -> num1 * num2
+            "/" -> {
+                if (num2 == 0.0) {
+                    throw ArithmeticException("No se puede dividir entre 0")
+                } else {
+                    num1 / num2
+                }
+            }
+
+            else -> throw IllegalArgumentException("Operador no válido: $operador")
         }
     }
 
@@ -59,8 +79,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun clear() {
-        num1 = 0
-        num2 = 0
+        num1 = 0.0
+        num2 = 0.0
         textPanttalla = ""
         binding.txtPantalla.text = "0"
         operacion = ""
@@ -75,6 +95,5 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
-
 }
+
